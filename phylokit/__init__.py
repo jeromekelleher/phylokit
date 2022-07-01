0952c2b069875cce4e1c79bf144c0758cfcf15b5
Jerome Kelleher <jk@well.ox.ac.uk>
2022-07-01 15:50:19 +0100

    Added initial dataset and UPGMA inference

--- phylokit/__init__.py
+++ phylokit/__init__.py
@@ -2,5 +2,14 @@ from .balance import b1_index  # NOQA
 from .balance import b2_index  # NOQA
 from .balance import colless_index  # NOQA
 from .balance import sackin_index  # NOQA
+from .core import tskit_to_dataset  # NOQA
+from .core import dataset_to_tskit  # NOQA
 
-__all__ = ["sackin_index", "colless_index", "b1_index", "b2_index"]
+__all__ = [
+    "sackin_index",
+    "colless_index",
+    "b1_index",
+    "b2_index",
+    "tskit_to_dataset",
+    "dataset_to_tskit",
+]
--- phylokit/core.py
+++ phylokit/core.py
@@ -1,4 +1,12 @@
 import numba
+import xarray
+import tskit
+import numpy as np
+
+DIM_NODE = "nodes"
+# Following sgkit example, specifically so that we can join on the samples dimension
+DIM_SAMPLE = "samples"
+
 
 DEFAULT_NUMBA_ARGS = {
     "nopython": True,
@@ -8,3 +16,42 @@ DEFAULT_NUMBA_ARGS = {
 
 def numba_njit(func, **kwargs):
     return numba.jit(func, **{**DEFAULT_NUMBA_ARGS, **kwargs})
+
+
+def create_tree_dataset(parent, time, left_child, right_sib, samples):
+    data_vars = {
+        "node_parent": ([DIM_NODE], parent),
+        "node_time": ([DIM_NODE], time),
+        "node_left_child": ([DIM_NODE], left_child),
+        "node_right_sib": ([DIM_NODE], right_sib),
+        "sample_node": ([DIM_SAMPLE], samples),
+    }
+    return xarray.Dataset(data_vars)
+
+
+def tskit_to_dataset(tree):
+    ts = tree.tree_sequence
+    # NOTE: we need to add an extra element here to keep the arrays in the nodes
+    # dimension the same length
+    time = np.append(ts.tables.nodes.time, np.inf)
+    return create_tree_dataset(
+        parent=tree.parent_array,
+        time=time,
+        left_child=tree.left_child_array,
+        right_sib=tree.right_sib_array,
+        samples=ts.samples(),
+    )
+
+
+def dataset_to_tskit(ds):
+    tables = tskit.TableCollection(1)
+    N = ds.sizes["nodes"] - 1
+    flags = np.zeros(N, dtype=np.uint32)
+    flags[ds.sample_node.to_numpy()] = tskit.NODE_IS_SAMPLE
+    tables.nodes.set_columns(flags=flags, time=ds.node_time[:-1])
+    # TODO do this more efficiently.
+    for u, parent in enumerate(ds.node_parent):
+        if parent != -1:
+            tables.edges.add_row(0, 1, parent, u)
+    tables.sort()
+    return tables.tree_sequence().first()
